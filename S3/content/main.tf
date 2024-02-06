de50f326c7520a7182bf4123e4841beb79105fe1
MITHUN <82029184+mythun-git@users.noreply.github.com>
2024-02-06 19:48:34 +0530

    Update main.tf

--- S3/content/main.tf
+++ S3/content/main.tf
@@ -10,6 +10,6 @@ resource "aws_s3_access_point" "${{ values.accessPointName }}" {
   name          = "${{values.accessPointName}}"
   bucket        = aws_s3_bucket.${{values.bucketName}}.bucket 
   vpc_configuration {
-    vpc_id = "${{values.vpcId}}
+    vpc_id = "${{values.vpcId}}"
   }
 }
