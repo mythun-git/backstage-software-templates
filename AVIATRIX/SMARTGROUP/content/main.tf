1dfb0ec6ddb8640744749be3a5e078eedb441e40
MITHUN <82029184+mythun-git@users.noreply.github.com>
2024-02-06 19:58:44 +0530

    Create main.tf

---
+++ AVIATRIX/SMARTGROUP/content/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aviatrix = {
+      source = "AviatrixSystems/aviatrix"
+      version = "3.1.4"
+    }
+  }
+}
+
+provider "aviatrix" {
+   controller_ip           = "${{ values.controllerIP }}"
+   username                = "${{ values.userName }}"
+   password                = "${{ values.password }}"
+   skip_version_validation = true
+ }
+
+# Create an Aviatrix Smart Group
+resource "aviatrix_smart_group" "${{ values.smartGroupName }}_ip" {
+  name = "${{ values.smartGroupName }}"
+  selector {
+    match_expressions {
+      type         = "vpc"
+      account_name = "${{ values.smartGroupAccName }}"
+      name = "${{ values.smartGroupName }}"
+    }
+  }
+}
