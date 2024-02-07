ed261c3ce8830a9792967640b3be643645b9df7f
PavanKumarAS <96346839+PavanKumarAS@users.noreply.github.com>
2024-02-07 13:28:34 +0530

    Update main.tf

--- AVIATRIX/TRANSIT_GATEWAT/content/main.tf
+++ AVIATRIX/TRANSIT_GATEWAT/content/main.tf
@@ -5,12 +5,12 @@ provider "aws" {
 
 locals {
   name   = "ex-tgw-${replace(basename(path.cwd), "_", "-")}"
-  region = "us-east-1"
+  region = "${{ values.region }}"
 
   tags = {
     Example    = local.name
-    GithubRepo = "terraform-aws-eks"
-    GithubOrg  = "terraform-aws-transit-gateway"
+    GithubRepo = "${{ values.gitHubRepo }}"
+    GithubOrg  = "${{ values.gitHubOrg }}"
   }
 }
 
@@ -23,41 +23,41 @@ module "tgw" {
   version = "~> 2.0"
 
   name            = local.name
-  description     = "My TGW shared with several other AWS accounts"
-  amazon_side_asn = 64532
+  description     = "${{ values.tgwDescription}}"
+  amazon_side_asn = ${{ values.amazonSideAsn }}
 
-  transit_gateway_cidr_blocks = ["10.99.0.0/24"]
+  transit_gateway_cidr_blocks = ["${{ cidrBlocks }}"]
 
   # When "true" there is no need for RAM resources if using multiple AWS accounts
-  enable_auto_accept_shared_attachments = true
+  enable_auto_accept_shared_attachments = ${{ values.enableAutoAcceptAttachments }}
 
   # When "true", allows service discovery through IGMP
-  enable_multicast_support = false
+  enable_multicast_support = ${{ values.enableMulticastSupport }}
 
   vpc_attachments = {
     vpc = {
       vpc_id       = module.vpc.vpc_id
       subnet_ids   = module.vpc.private_subnets
-      dns_support  = true
+      dns_support  = ${{ values.vpcAttachDnsSupport }}
       ipv6_support = true
 
-      transit_gateway_default_route_table_association = false
-      transit_gateway_default_route_table_propagation = false
+      transit_gateway_default_route_table_association = ${{ vlaues.tgDefaultRouteTableAssociation }}
+      transit_gateway_default_route_table_propagation = ${{ vlaues.tgDefaultRouteTablePropagation }}
 
       tgw_routes = [
         {
-          destination_cidr_block = "30.0.0.0/16"
+          destination_cidr_block = "${{ values.tgwRoute1 }}"
         },
         {
-          blackhole              = true
-          destination_cidr_block = "0.0.0.0/0"
+          blackhole              = ${{ values.tgwBlackhole }}
+          destination_cidr_block = "${{ values.tgwRoute2 }}"
         }
       ]
     }  
   }
 
-  ram_allow_external_principals = true
-  ram_principals                = [307990089504]
+  ram_allow_external_principals = ${{ values.ramAllowExternalPrincipals }}
+  ram_principals                = [ ${{ values.ramPrincipals }} ]
 
   tags = local.tags
 }
@@ -71,13 +71,13 @@ module "vpc" {
   version = "~> 5.0"
 
   name = "${local.name}-vpc"
-  cidr = "10.10.0.0/16"
+  cidr = "${{ values.vpcCidr }}"
 
   azs             = ["${local.region}a", "${local.region}b", "${local.region}c"]
   private_subnets = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
 
-  enable_ipv6                                    = true
-  private_subnet_assign_ipv6_address_on_creation = true
+  enable_ipv6                                    = ${{ values.enableIpv6 }}
+  private_subnet_assign_ipv6_address_on_creation = ${{ values.assignIpv6AddressOnCreation }}
   private_subnet_ipv6_prefixes                   = [0, 1, 2]
 
   tags = local.tags
