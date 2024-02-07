7fe43c6b66361631dd4355ea55bbb54fe199fb4e
MITHUN <82029184+mythun-git@users.noreply.github.com>
2024-02-07 11:45:29 +0530

    Update main.tf

--- PG/content/main.tf
+++ PG/content/main.tf
@@ -4,20 +4,20 @@ provider "aws" {
 # Specify your existing VPC ID
 variable "vpc_id" {
   type    = string
-  default = "vpc-005aa4d379e350404"
+  default = "${{ values.vpcId }}"
 }
 
 # Specify your existing security group ID
 variable "security_group_id" {
   type    = string
-  default = "sg-0a632163f5063faa6"
+  default = "${{ values.securityGroupId }}"
 }
 
 # Specify your existing subnet group ID
 resource "aws_db_instance" "my_${{ values.dbIdentifierName}}" {
-    identifier             = "${{ values.dbIdentifierName}}"
-    instance_class         = "db.t3.micro"
-    allocated_storage      = 20
+    identifier             = "${{ values.dbIdentifierName }}"
+    instance_class         = "${{ values.instanceClass }}"
+    allocated_storage      = ${{ values.storageSize }}
     db_name                = "${{ values.databaseName}}"
     storage_type           = "gp2"
     engine                 = "postgres"
