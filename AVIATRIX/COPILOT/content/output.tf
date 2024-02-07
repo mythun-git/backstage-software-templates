dfba1600437994b8b3ddb6c89a25540594c8eb9b
MITHUN <82029184+mythun-git@users.noreply.github.com>
2024-02-07 17:16:03 +0530

    Create output.tf

---
+++ AVIATRIX/COPILOT/content/output.tf
@@ -0,0 +1,29 @@
+output "ec2-info" {
+  value       = aws_instance.aviatrixcopilot.*
+  description = "EC2 instance info"
+}
+
+output "private_ip" {
+  value       = aws_instance.aviatrixcopilot.private_ip
+  description = "Private IP address of the Aviatrix Copilot"
+}
+
+output "public_ip" {
+  value       = concat(aws_eip.copilot_eip.*.public_ip, [null])[0]
+  description = "Public IP address of the Aviatrix Copilot"
+}
+
+output "vpc_id" {
+  value       = var.vpc_id
+  description = "VPC ID"
+}
+
+output "vpc_name" {
+  value       = var.tags
+  description = "VPC name"
+}
+
+output "region" {
+  value       = data.aws_region.current.name
+  description = "Current AWS region"
+}
